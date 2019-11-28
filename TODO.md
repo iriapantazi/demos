74f9cef1f888bc6e66bcfab3faf55f86c9a5fb5c
Iria Pantazi <iria.a.pantazi@gmail.com>
2019-11-28 17:45:59 +0000

    logging and status codes

--- TODO.md
+++ TODO.md
@@ -1,12 +1,14 @@
 # Iria tasks
 ## Primary
-- replace print statements with log statements. Investigate the use of the "logging" library that comes with Python.
+- replace print statements with log statements. Investigate the use of the "logging" library that comes with Python. [OK]
 - the code uses hardcoded values. can we make it configurable?
 - time diffing is complicated. Can we simplify all the logic (unix timestamps)?
   - compareTime: no input validation, no exception handling.
   - checkLastTimeExecutedRequests: wrong documentation.
   - getDateTimeFormatted: too elaborate.
 
+- get severity codes from API once and for all. hence avoid hard-coding for assigning colors.
+- tox package
 
 ## Secondary
 - write a small paragraph why logging is preferable to print statements.
--- trainsLinesStatus.py
+++ trainsLinesStatus.py
@@ -6,25 +6,31 @@ import json
 import sys
 import os
 import datetime
+import time
 from colorama import Fore
-import pandas as pd
+import csv
+import logging
 
 parser = argparse.ArgumentParser(
-            description="This program prints the\
-            train lines, i.e. tube, dlr, tram, overground\
-            of London at the time that the program is executed.\
-            The valid modes are only: tube, tram, dlr,\
-            overground. Enter the desired one(s) after the\
-            flag -m.")
+            description="This program prints the"
+            " train lines, i.e. tube, dlr, tram, overground"
+            " of London at the time that the program is executed."
+            " The valid modes are only: tube, tram, dlr,"
+            " overground. Enter the desired one(s) after the"
+            " flag -m, or use -a for all modes. Flag -f forces"
+            " the request even if not enough time has passed.")
 parser.add_argument('-m', '--modes', nargs='+', 
                     help='entrer the train mode(s)')
 parser.add_argument('-a', '--all_modes', action='store_true',
                     help='for tube lines, DLR, tram, overground')
 parser.add_argument('-f', '--force', action='store_true',
                     help='force request, and also include'
-                    'the desired modes to')
+                    'the desired mode(s)')
 args = parser.parse_args()
 
+logging.basicConfig(filename='tfl_requests.log',
+        filemode='w',
+        format='%(asctime)s--%(levelname)s--%(message)s')
 
 TFLAPI = 'https://api.tfl.gov.uk'
 HEADERS = {'Accept': 'application/json; charset=utf-8'}
@@ -33,9 +39,9 @@ SAVEFILE = 'outputLinesStatus.csv'
 
 def detectPythonVersion():
     """
-    detectPythonVersion: Args: - Returns: -
-    This module checks if the user 
-    is using Python2.x. and raises exception.
+    detectPythonVersion: 
+    Args: - 
+    Returns: -
     """
     if sys.version_info.major < 3:
         message = 'Python' + str(sys.version_info.major) +\
@@ -47,104 +53,101 @@ def detectPythonVersion():
 
 def checkLastTimeExecutedRequests():
     """
-    checkLastTimeExecutedRequests: Args: - Returns: -
+    checkLastTimeExecutedRequests: 
+    Args: - 
+    Returns: -
     This file checks the last time that a requests.get
-    was sent to the server, and if it was within 10 min
-    then, the file that keeps the status for each line
-    will be called, and the information will be printed
-    on the terminal. If 10 minutes have passed, then a
-    new request will be sent to the server.
+    was sent to the server. If it was within 5 min
+    the status will be read from the SAVEFILE.
     """
     toRequest = False
-    nowDate, nowTime = getDateTimeFormatted()
+    nowTime = int(time.time())
     try:
         with open(SAVEFILE, 'r') as f:
-            line = f.readline()
-            thenDate, thenTime = line.split(',')
-            if (nowDate != thenDate):
-                print('Detected different date, will request.')
+            thenTime = int(f.readline())
+            if (nowTime - thenTime > 300):
                 toRequest = True
-            else:
-                toRequest = compareTime(thenTime, nowTime)
     except FileNotFoundError as err:
-        print(err)
-        print('No file found, will request.')
+        logging.warning('No file found, will request.')
         toRequest = True
     except ValueError as err:
-        print(err)
-        print('The code will abort.')
+        logging.warning('No valid format of SAVEFILE, '
+                'will be considered as FileNotFoundError.')
+        toRequest = True
+    except Exception as e:
+        loggind.error(e)
+        print(e)
+        print('Aborting.')
         sys.exit(-1)
     return(toRequest)
 
 
-def getDateTimeFormatted():
-    nowTime = datetime.datetime.now()
-    date = str('{}-{}-{}'.format(nowTime.year, nowTime.month, nowTime.day))
-    time = str('{}:{}'.format(nowTime.hour, nowTime.minute))
-    return(date, time)
-
-
-def compareTime(thenTime, nowTime):
-    thenHr, thenMin = thenTime.split(':')
-    nowHr, nowMin = nowTime.split(':')
-    if (thenHr != nowHr):
-        print('Different hour, will request.')
-        toRequest = True
-    elif (int(nowMin) - int(thenMin) > 5):
-        print('More than 5 min difference, will request.')
-        toRequest = True
-    else:
-        print('No need to request, not enough time passed.')
-        toRequest = False
-    return(toRequest)
+def associateStatusColor(status):
+    """
+    associateStatusColor:
+    Args: status [str]
+    Return: clr [ANSI]
+    This function assigns colors for
+    different line status.
+    """
+    #validCodes = list(range(0, 20))
+    #if (status == 'Good Service'):
+    #    clr = Fore.CYAN
+    #elif (status == 'Minor Delays'):
+    #    clr = Fore.RED
+    #elif (status == 'Severe Delays'):
+    #    clr = Fore.GREEN
+    #elif (status == 'Planned Closure'):
+    #    clr = Fore.YELLOW
+    #elif (status == 'Part Closure'):
+    #    clr = Fore.MAGENTA
+    #elif (status == 'Part Suspended'):
+    #    clr = Fore.GREEN
+    #else:
+    #    clr = Fore.BLUE
+    clr = Fore.BLUE
+    return(clr)
 
 
 def printFormattedData():
     """
-    printFormattedData: Args: - Returns: -
-    this function reads the csv file named SAVEFILE
-    and the status of each line is printed with
-    the appropriate color.
+    printFormattedData: 
+    Args: - 
+    Returns: -
+    This function reads the csv file named SAVEFILE
+    and the status of each line is printed in the
+    standard output.
     """
-    data = pd.read_csv(SAVEFILE, names = ['lineName', 'lineStatus'],
-                       header = 0)
-    for i in range(len(data)):
-        tempName = data['lineName'][i]
-        tempStat = data['lineStatus'][i]
-        if (tempStat == 'Good Service'):
-            clr = Fore.CYAN
-        elif (tempStat == 'Minor Delays'):
-            clr = Fore.RED
-        elif (tempStat == 'Severe Delays'):
-            clr = Fore.GREEN
-        elif (tempStat == 'Planned Closure'):
-            clr = Fore.YELLOW
-        elif (tempStat == 'Part Closure'):
-            clr = Fore.MAGENTA
-        elif (tempStat == 'Part Suspended'):
-            clr = Fore.GREEN
-        else:
-            clr = Fore.BLUE
-        print(('{} Line reports ' + clr +\
-              '{} \033[0m').format(tempName, tempStat))
+    with open(SAVEFILE, 'r') as f:
+        # to skip 1st line that includes timestamp
+        time = f.readline()
+        f_reader = csv.reader(f, delimiter=',') 
+        for row in f_reader:
+            rowName = row[0]
+            rowCode = row[1]
+            rowDesc = row[2]
+            clr = associateStatusColor(rowCode)
+            print(('{} Line reports ' + clr +\
+                    '{} \033[0m').format(rowName, rowDesc))
 
 
 def requestStatusFromServer(url):
     """
-    requestStatusFromServer: Args: url [string]
-    Returns: [list of dictionary]
+    requestStatusFromServer: 
+    Args: url [string]
+    Returns: data [list]
     This module will request information
     on lines status and will return then
     in the form of list contaning a dictionary.
     """
     r = requests.get(url, headers=HEADERS)
     if not r.ok:
-        print('Exits with request status code'
+        logging.error('Exits with request status code'
               '{} on {}.'.format(r.status_code, url))
         sys.exit(-1)
     data = r.json()
     if (type(data) != list or len(data) < 1):
-        print('requestFromServer: Received not list,'
+        logging.error('requestFromServer: Received not list,'
               'or empty list of status for lines, Aborting.')
         sys.exit(-1)
     return(data)
@@ -154,16 +157,17 @@ def writeAndSaveData(data):
     """
     """
     with open(SAVEFILE, 'w') as f:
-        date, time = getDateTimeFormatted()
-        f.write('{},{}\n'.format(date, time))
+        nowTime = int(time.time())
+        f.write('{}\n'.format(nowTime))
         for status in data:
             if (type(status) != dict):
-                print('requestFromServer: contact me')
+                logging.error('requestFromServer: contact me')
                 sys.exit(-1)
             lineName = status.get('name')
             lineStatus = status.get('lineStatuses')[0]
-            lineStatus = lineStatus.get('statusSeverityDescription')
-            f.write('{},{}\n'.format(lineName, lineStatus))
+            statusCode = lineStatus.get('statusSeverity')
+            statusDescription = lineStatus.get('statusSeverityDescription')
+            f.write('{},{},{}\n'.format(lineName, statusCode, statusDescription))
 
 
 def createStatusURL(modes):
@@ -174,9 +178,8 @@ def createStatusURL(modes):
     """
     try:
         url = '{}/line/mode/{}/status'.format(TFLAPI, modes)
-    except:
-        
-        pass
+    except Exception as e:
+        logging.error(e)    
     return(url)
 
 
@@ -192,12 +195,14 @@ def returnValidModesString(modes):
         request_modes.append('tube')
     if 'overground' in modes:
         request_modes.append('overground')
+    if 'tflrail' in modes:
+        request_modes.append('tflrail')
     if 'tram' in modes:
         request_modes.append('tram')
     if 'dlr' in modes:
         request_modes.append('dlr')
     if (len(request_modes) < 1 or type(request_modes) != list):
-        print('returnValidModesString: received no valid modes.'
+        logging.warning('returnValidModesString: received no valid modes.'
               ' Aborting.')
         sys.exit(-1)
     request_modes = ','.join(request_modes)
@@ -206,12 +211,13 @@ def returnValidModesString(modes):
 
 def returnAllValidModesString():
     """
-    returnAllValidModesString: Args: - 
+    returnAllValidModesString: 
+    Args: - 
     Returns: all valid train modes.
     This is hard coded to include only
     dlr, tram, tube, overgound.
     """
-    request_modes = 'tube,tram,overground,dlr'
+    request_modes = 'tube,tram,overground,dlr,tflrail'
     return(request_modes)
 
 
@@ -235,7 +241,7 @@ def main():
         data = requestStatusFromServer(statusURL)
         writeAndSaveData(data)
     else:
-        print('No request. Reads output from file.')
+        logging.warning('No request. Reads output from file.')
     printFormattedData()
 
 
