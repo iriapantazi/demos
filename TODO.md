ce5263bb0e1e405e9ea9f356b2b7aab4e2f7b6a9
Iria Pantazi <iria.a.pantazi@gmail.com>
2019-12-12 18:30:22 +0000

    fixed logger added validation

--- TODO.md
+++ TODO.md
@@ -15,33 +15,43 @@
 - may be some validation cases not considered by developer
 - maybe use patch when needing in a test function that requires 
   a local variable from the corresponding function.
-- which of two versions for writeAndSaveData to keep?
 - what validation to include in associateStatusColor ?
 - In the tests do I have to test individually every Exception that can be raised?
 - why './tests/test_trainsLinesStatus.py' does not work, while pytest works
 - cannot import modules error has to do sth w/ __init__ and directories?
-
-#Questions to myself
 - How can I test the argument parser?
-- How can I print the parser help when user parses invalid arguments?
 
 # Iria tasks (by Leonidas)
-NB The ones I did are marked with [OK], otherwise with [-].
-
 ## Primary
-- fix requirements.txt and requirements-dev.txt. Create new env and start from scratch. 
-- read the documentation of setuptools.py. what does find_packages() do inside my setup.py?
+- fix requirements.txt and requirements-dev.txt. 
+  Create new env and start from scratch. 
+
+- read the documentation of setuptools.py. 
+  what does find_packages() do inside my setup.py?
+
 - implement cli argument to control logging level.
 
 - since we use getattr, could you showcase the use of hasattr() and
   setattr()? Also, we do not error check where we call getattr.
 
+
+# Iria Done
+---
+
+- fixed name conventions 
+
 - the code uses hardcoded values. can we make it configurable? [NOT DONE]
-  LEONIDAS: afto to 300 ekei den einai 
+  LEONIDAS: afto to 300 ekei den einai [iria DONE] 
 
-- writeAndSaveData(): can we split it up in more functions? it should make things more easily tested.
+- Replaced str.format() with f-strings
 
-- time diffing is complicated. Can we simplify all the logic (unix timestamps)? [OK]
+- How can I print the parser help when user parses invalid arguments?
+
+- writeAndSaveData(): can we split it up in more functions? 
+  it should make things more easily tested.
+
+- time diffing is complicated. 
+  Can we simplify all the logic (unix timestamps)? [OK]
   - compareTime: no input validation, no exception handling. [OK]
   - checkLastTimeExecutedRequests: wrong documentation. [OK]
   - getDateTimeFormatted: too elaborate. [OK]
--- trainsLinesStatus.py
+++ trainsLinesStatus.py
@@ -13,13 +13,13 @@ import logging
 
 
 parser = argparse.ArgumentParser(
-            description="This program prints the"
-            " train lines, i.e. tube, etc."
-            " of London at the time that the program is executed."
-            " The valid modes are only: tube, tram, dlr, tflrail,"
-            " overground. Enter the desired one(s) after the"
-            " flag -m, or use -a for all modes. Flag -f forces"
-            " the request even if not enough time has passed.")
+            description='This program prints the'
+            ' train lines, i.e. tube, etc.'
+            ' of London at the time that the program is executed.'
+            ' The valid modes are only: tube, tram, dlr, tflrail,'
+            ' overground. Enter the desired one(s) after the'
+            ' flag -m, or use -a for all modes. Flag -f forces'
+            ' the request even if not enough time has passed.')
 parser.add_argument('-m', '--modes', nargs='+', 
                     help='entrer the train mode(s)')
 parser.add_argument('-a', '--all_modes', action='store_true',
@@ -28,37 +28,65 @@ parser.add_argument('-a', '--all_modes', action='store_true',
 parser.add_argument('-f', '--force', action='store_true',
                     help='force request, and also include'
                     'the desired mode(s)')
+parser.add_argument('-t', '--time_to_leave', nargs='?', 
+        const='300', type=int, 
+        help='perform request after these seconds')
+parser.add_argument('-o', '--output', nargs='?', 
+        const='output_lines_status.csv', type=str, 
+        help='perform request after these seconds')
+parser.add_argument('-l', '--logging_level', nargs='?', 
+        const='WARNING', help='set the logging level')
 args = parser.parse_args()
-
-
-logging.basicConfig(filename='tfl_requests.log',
-        filemode='w',
-        format='%(asctime)s--%(levelname)s--%(message)s')
 
 
 TFLAPI = 'https://api.tfl.gov.uk'
 HEADERS = {'Accept': 'application/json; charset=utf-8'}
-SAVEFILE = 'outputLinesStatus.csv'
+if (args.output):
+    SAVEFILE = args.output
+else:
+    SAVEFILE = 'output_lines_status.csv'
 
 
-def detectPythonVersion():
+def configure_logger(lvl='WARNING'):
     """
-    detectPythonVersion: 
+    configure_logger:
+    Args:
+    Returns: -
+    This function configures the logger
+    """
+    try:
+        log_lvl = getattr(logging, lvl)
+    except AttributeError:
+        log_lvl = getattr(logging, 'WARNING')
+    logging.basicConfig(filename='tfl_requests.log',
+            level=log_lvl, filemode='w',
+            format='%(asctime)s--%(levelname)s--%(message)s')
+
+
+def detect_python_version():
+    """
+    detect_python_version:
     Args: - 
     Returns: -
     """
     if sys.version_info.major < 3:
-        message = 'Python' + str(sys.version_info.major) +\
-                  '.' + str(sys.version_info.minor) +\
-                  ' is retired or about to retire. ' +\
-                  'Please update.'
-        logging.error(message)
-        raise Exception(message)
+        msg = (f'Python {sys.version_info.major}.'
+                f'{sys.version_info.minor} '
+                f'is retired or about to retire. '
+                f'Please update.')
+        logging.error(msg)
+        raise Exception(msg)
+    elif sys.version_info.minor < 6:
+        msg = (f'Python {sys.version_info.major}.'
+                f'{sys.version_info.minor} '
+                f' may not work properly.')
+        logging.warning(msg)
 
 
-def checkLastTimeExecutedRequests():
+
+def check_last_time_executed_requests(ttl=300):
     """
-    checkLastTimeExecutedRequests: 
+    check_last_time_executed_requests:
     Args: - 
     Returns: toRequest [bool]
     This file checks the last time that a requests.get
@@ -70,26 +98,24 @@ def checkLastTimeExecutedRequests():
     try:
         with open(SAVEFILE, 'r') as f:
             thenTime = int(f.readline())
-            if (nowTime - thenTime > 300):
+            if (nowTime - thenTime > ttl):
                 toRequest = True
     except FileNotFoundError as err:
         logging.warning('No file found, will request.')
         toRequest = True
     except ValueError as err:
-        logging.warning('No valid format of SAVEFILE, '
-                'will be considered as FileNotFoundError.')
+        logging.warning(f'No valid format of {SAVEFILE}, '
+                f'will be considered as FileNotFoundError.')
         toRequest = True
     except Exception as e:
-        loggind.error(e)
-        print(e)
-        print('Aborting.')
+        logging.critical(e)
         sys.exit(-1)
     return(toRequest)
 
 
-def associateStatusColor(rowCode):
+def associate_status_color(rowCode):
     """
-    associateStatusColor:
+    associate_status_color:
     Args: status [str]
     Return: clr [str]
     This function assigns colors in ANSI format
@@ -98,13 +124,17 @@ def associateStatusColor(rowCode):
     rowCode = rowCode % 6
     colors = ['BLUE', 'CYAN', 'RED', 'MAGENTA', 'GREEN', 'YELLOW']
     color = colors[rowCode]
-    clr = getattr(Fore, color)
+    try:
+        clr = getattr(Fore, color)
+    except Exception as e:
+        logging.error(f'Error with the color attributes: {e}')
+        clr = getattr(Fore, 'RESET')
     return(clr)
 
 
-def printFormattedData():
+def print_saved_data():
     """
-    printFormattedData: 
+    print_saved_data:
     Args: - 
     Returns: -
     This function reads from SAVEFILE the name(s)
@@ -120,17 +150,17 @@ def printFormattedData():
                 rowName = row[0]
                 rowCode = int(row[1])
                 rowDesc = row[2]
-                clr = associateStatusColor(rowCode)
+                clr = associate_status_color(rowCode)
             except Exception as e:
                 logging.error(e)
                 sys.exit(-1)
-            print(('{} Line reports ' + clr +\
-                    '{}' + Fore.RESET).format(rowName, rowDesc))
+            print(f'{rowName} Line reports {clr} '
+                    f'{rowDesc} {Fore.RESET}')
 
 
-def requestStatusFromServer(url):
+def request_status_from_server(url):
     """
-    requestStatusFromServer: 
+    request_status_from_server:
     Args: url [string]
     Returns: data [list]
     This module will request information
@@ -139,20 +169,41 @@ def requestStatusFromServer(url):
     """
     r = requests.get(url, headers=HEADERS)
     if not r.ok:
-        logging.error('Exits with request status code'
-              '{} on {}.'.format(r.status_code, url))
+        logging.critical(f'Exits with request status code '
+                f'{r.status_code} on {url}.')
         sys.exit(-1)
     data = r.json()
     if (type(data) != list or len(data) < 1):
-        logging.error('requestFromServer: Received not list, '
-              'or empty list of status for lines, Aborting.')
+        logging.critical(f'request_status_from_server: '
+                f'Received not list, or empty list. '
+                f'Aborting.')
         sys.exit(-1)
     return(data)
 
 
-def writeAndSaveData(data):
+def get_dictionary_key(theDict, theKey):
     """
-    writeAndSaveData:
+    get_dictionary_key:
+    Args: theDict [dict], theKey [str]
+    Returns: ?
+    This function asserts that the key exists
+    in the dictionary, and returns the value. 
+    If it does not, it logs an error and exits
+    with the appropriate message.
+    """
+    try: 
+        theValue = theDict.get(theKey)
+    except KeyError:
+        msg = f'Dictionary {theDict} contains no key {theKey}.'
+        logging.error(msg)
+        sys.exit(-1)
+    else:
+        return(theValue)
+
+
+def write_save_data(data):
+    """
+    write_save_data:
     Args: data [list]
     Returns: -
     This function opens a file where the output will 
@@ -161,53 +212,46 @@ def writeAndSaveData(data):
     """
     with open(SAVEFILE, 'w') as f:
         nowTime = int(time.time())
-        f.write('{}\n'.format(nowTime))
+        f.write(f'{nowTime}\n')
+
         if type(data) != list: 
-            logging.error('Expected a list.')
+            logging.error(f'Expected {data} to be a list.')
             sys.exit(-1)
+
         for status in data:
             if (type(status) != dict):
-                logging.error('Expected a dictionary.')
+                logging.error(f'Expected {status} to be a dictionary.')
                 sys.exit(-1)
-            lineName = status.get('name')
-            if lineName == None:
-                logging.error('No such key found in dictionary ' 
-                        + str(status))
-                sys.exit(-1)
-            lineStatus = status.get('lineStatuses')[0]
-            statusCode = lineStatus.get('statusSeverity')
-            if statusCode == None:
-                logging.error('No such key found in dictionary ' 
-                        + str(status))
-                sys.exit(-1)
-            statusDescription = \
-                    lineStatus.get('statusSeverityDescription')
-            if statusDescription == None:
-                logging.error('No such key found in dictionary ' +
-                        str(status))
-                sys.exit(-1)
-            f.write('{},{},{}\n'.format(lineName, 
-                statusCode, statusDescription))
+            lineName = get_dictionary_key(status, 'name')
+            lineStatus = get_dictionary_key(status, 
+                    'lineStatuses')[0]
+            statusCode = get_dictionary_key(lineStatus, 
+                    'statusSeverity')
+            statusDescription = get_dictionary_key(lineStatus, 
+                    'statusSeverityDescription')
+            f.write(f'{lineName},{statusCode},{statusDescription}\n')
 
 
-def createStatusURL(modes):
+def create_status_URL(modes):
     """
-    createStatusURL: 
+    create_status_URL:
     Args: modes [str]
     Returns: url [str]
     This function generates the appropriate url for
     requesting the status of valid modes.
     """
     try:
-        url = '{}/line/mode/{}/status'.format(TFLAPI, modes)
+        url = f'{TFLAPI}/line/mode/{modes}/status'
     except Exception as e:
         logging.error(e)    
     return(url)
 
 
-def returnValidModesString(modes):
+def return_valid_modes_string(modes):
     """
-    returnValideModesString:
+    return_valid_modes_string:
+    Args: modes [str]
+    Returns: request_modes [str]
     correction to accelerate testing of the modes
     validity. so that it rejects non-valid or
     non-implemented modes.
@@ -224,48 +268,95 @@ def returnValidModesString(modes):
     if 'dlr' in modes:
         request_modes.append('dlr')
     if (len(request_modes) < 1 or type(request_modes) != list):
-        logging.warning('returnValidModesString:'
-                ' received no valid modes.'
-                ' Aborting.')
+        logging.error(f'returnValidModesString: '
+                f'received no valid modes. Aborting.')
         sys.exit(-1)
     request_modes = ','.join(request_modes)
     return(request_modes)
 
 
-def returnAllValidModesString():
+def return_all_valid_modes_string():
     """
-    returnAllValidModesString: 
+    return_all_valid_modes_string:
     Args: - 
-    Returns: all valid train modes.
+    Returns: request_modes [str]
     This is hard coded to include only
     dlr, tram, tube, overgound, tflrail.
     """
-    request_modes = 'tube,tram,overground,dlr,tflrail'
+    request_modes = (f'tube,tram,overground,dlr,tflrail')
     return(request_modes)
+
+
+def set_logger():
+    """
+    set_logger:
+    Args: -
+    Returns: -
+    """
+
+    if (args.logging_level):
+        lvl = args.logging_level
+    else:
+        lvl = 'WARNING'
+    configure_logger(lvl)
+
+
+def set_ttl_toRequest():
+    """
+    set_ttl_toRequest:
+    Args: -
+    Returns: toRequest [bool]
+    """
+    if (args.time_to_leave):
+        ttl = args.time_to_leave
+    else:
+        ttl = 300
+
+    if (args.force):
+        toRequest = True
+    else:
+        toRequest = check_last_time_executed_requests(ttl)
+    return(ttl, toRequest)
+
+
+def execute_request(ttl, toRequest):
+    """
+    execute_request:
+    Args: -
+    Returns: -
+    """
+    if toRequest:
+        if (args.modes):
+            modesString = return_valid_modes_string(args.modes)
+        elif (args.all_modes):
+            modesString = return_all_valid_modes_string()
+        else:
+            parser.print_help()
+            sys.exit(-1)
+
+        statusURL = create_status_URL(modesString)
+        data = request_status_from_server(statusURL)
+        write_save_data(data)
+
+    else:
+        logging.warning(f'No request since less than {ttl} '
+                f'seconds have passed. Reads output from file.')
+
 
 
 def main():
     """
     """
-    detectPythonVersion()
-    if (args.force):
-        toRequest = True
-    else:
-        toRequest = checkLastTimeExecutedRequests()
-    if toRequest:
-        if (args.modes):
-            modesString = returnValidModesString(args.modes)
-        elif (args.all_modes):
-            modesString = returnAllValidModesString()
-        else:
-            parser.print_help()
-            sys.exit(-1)
-        statusURL = createStatusURL(modesString)
-        data = requestStatusFromServer(statusURL)
-        writeAndSaveData(data)
-    else:
-        logging.warning('No request. Reads output from file.')
-    printFormattedData()
+
+    set_logger()
+
+    detect_python_version()
+
+    ttl, toRequest = set_ttl_toRequest()
+
+    execute_request(ttl, toRequest)
+
+    print_saved_data()
 
 
 if __name__ == "__main__":
